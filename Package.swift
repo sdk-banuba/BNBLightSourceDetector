a835e527793d17cce633abbebd347a6b13ac84cf
ci <ci@banuba.com>
2024-12-12 17:49:16 +0300

    1.16.2

---
+++ Package.swift
@@ -0,0 +1,56 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.16.2"
+
+let package = Package(
+    name: "BNBLightSourceDetector",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BNBLightSourceDetector",
+            targets: [
+                "BNBLightSourceDetector",
+                "BNBLightSourceDetector_BNBSdkCore",
+                "BNBLightSourceDetector_BNBFaceTracker"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/sdk-banuba/BNBSdkCore.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/sdk-banuba/BNBFaceTracker.git",
+            .exact(bnbPackageVersion)
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BNBLightSourceDetector",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.16.2/BNBLightSourceDetector.zip",
+            checksum: "47bb162382edb530c124efb04d2b0d5dc6edd019bd6a0ef9dfccb50a590e48c9"
+        ),
+        .target(
+            name: "BNBLightSourceDetector_BNBSdkCore",
+            dependencies: [
+                .product(
+                    name: "BNBSdkCore",
+                    package: "BNBSdkCore"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBLightSourceDetector_BNBFaceTracker",
+            dependencies: [
+                .product(
+                    name: "BNBFaceTracker",
+                    package: "BNBFaceTracker"
+                ),
+            ]
+        ),
+    ]
+)
