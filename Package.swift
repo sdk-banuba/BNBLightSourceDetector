2a6ed6d21f82ba311d89bac9a6f36df0f08bec81
ci <ci@banuba.com>
2026-04-29 20:25:21 +0300

    1.18.1

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.18.0-8-g22fe376040"
+let bnbPackageVersion: Version = "1.18.1"
 
 let package = Package(
     name: "BNBLightSourceDetector",
@@ -31,8 +31,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBLightSourceDetector",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.18.0-8-g22fe376040/BNBLightSourceDetector.zip",
-            checksum: "72e9bd3a0a8a6aca8b1085c4a5a144e9078708d57c6cc21f0ad31dc473255e59"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.18.1/BNBLightSourceDetector.zip",
+            checksum: "8bdeb7948cef439ede763587639d91a9789d17663d864f0f793d4da54dafc69d"
         ),
         .target(
             name: "BNBLightSourceDetector_BNBSdkCore",
